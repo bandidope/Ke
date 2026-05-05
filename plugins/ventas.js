e58364b6904f6825bbf251fc33fd63bdb184a74f
Whois1020 <whoisgithun@gmail.com>
2026-05-04 20:23:49 -0500

    Add files via upload

---
+++ plugins/ventas.js
@@ -0,0 +1,28 @@
+import fg from 'api-dylux' 
+import fetch from 'node-fetch'
+import axios from 'axios'
+let handler = async (m, { conn, args, command, usedPrefix }) => {
+if (!args[0]) throw `
+Owners / Vendedores Chelzzy 🫶🏻
+
+Owner:
+: ̗̀➛ 👑Whois : Wa.me/51936994155
+
+Vendedores:
+: ̗̀➛ 👸🏻Melisa : Wa.me/51993800613
+: ̗̀➛ 👸🏻 Thali : Wa.me/+573042479614
+: ̗̀➛ 👸🏻 Elizabeth : Wa.me/51960831611
+: ̗̀➛ 👸🏻 TodoBenne' : Wa.me/56931300864
+: ̗̀➛ 👸🏻 Yane : Wa.me/529813279747
+
+Grupo Oficial:
+: ̗̀➛ https://chat.whatsapp.com/Fi6FHZ8VSGnAT7CKJkcd9r
+                 
+` 
+}
+handler.help = ['owner']
+handler.tags = ['owner']
+handler.command = /^(owner)$/i
+handler.group = true
+handler.admin = false
+export default handler
