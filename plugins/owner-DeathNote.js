5aea5b158512a3644ff7611a16cf5983c14fcaf0
YALLICO OWNER <yallico2024@gmail.com>
2026-05-27 01:05:12 -0500

    Actualizar owner-DeathNote.js

--- plugins/owner-DeathNote.js
+++ plugins/owner-DeathNote.js
@@ -1,18 +1,16 @@
 import { spawn } from 'child_process'
-
-var handler = async (m, { conn, isROwner, text }) => {
-
-if (!process.send) throw 'Dont: node mini.js\nDo: node index.js'
-if (conn.user.jid == conn.user.jid) {
-await conn.reply(m.chat, '𝐒𝐞𝐫𝐯𝐢𝐝𝐨𝐫 𝐑𝐞𝐢𝐧𝐢𝐜𝐢𝐚𝐝𝐨 𝐂𝐨𝐧 𝐄𝐱𝐢𝐭𝐨✅\n𝐕𝐮𝐞𝐥𝐯𝐞 𝐀 𝐔𝐬𝐚𝐫𝐥𝐨 𝐃𝐞𝐧𝐭𝐫𝐨 𝐃𝐞 𝟏𝟓𝐬 🔥\n𝐄𝐚𝐳𝐳𝐲 𝐗 💨', m, rcanal, )
-process.send('reset')
-} else throw 'eh'
-
+let handler = async (m, { conn, isROwner, text }) => {
+    if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
+    if (conn.user.jid == conn.user.jid) {
+    await m.reply('🍭 Reiniciando Bot...')
+    process.send('reset')
+  } else throw 'eh'
 }
-handler.help = ['restart']
+
+handler.help = ['reiniciar']
 handler.tags = ['owner']
 handler.command = ['restart','reiniciar'] 
 
-handler.rowner = false
+handler.rowner = true
 
 export default handler
