fa37c013899d857ab945dda3c050f4cbb96231f4
YALLICO OWNER <yallico2024@gmail.com>
2026-05-27 20:15:46 -0500

    Actualizar main-runtime.js

--- plugins/main-runtime.js
+++ plugins/main-runtime.js
@@ -9,7 +9,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
       }) * 1000
     }
     let muptime = clockString(_muptime)
-   conn.reply(m.chat, `*🍭 He estado activa durante :* ${muptime}`, m)
+   m.reply(`*» Bot activo durante* : ${muptime}`) 
 }
 handler.help = ['runtime']
 handler.tags = ['main']
@@ -20,9 +20,6 @@ function clockString(ms) {
   let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000)
   let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
   let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [d, 'd ', h, 'h ', m, 'm ', s, 's '].map(v => v.toString().padStart(2, 0)).join('')
-}
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
   return [d, 'd ', h, 'h ', m, 'm ', s, 's '].map(v => v.toString().padStart(2, 0)).join('')
 }
