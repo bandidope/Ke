92ec90b79708483202a9e7375445d42526e04649
YALLICO OWNER <yallico2024@gmail.com>
2026-05-29 10:34:07 -0500

    Actualizar tools-bratvideo.js

--- plugins/tools-bratvideo.js
+++ plugins/tools-bratvideo.js
@@ -1,21 +1,28 @@
-            case 'bratv': {
-                text = m.quoted?.text || text
-                if (!text) return conn.sendMessage(m.chat, {
-                    text: `> . ﹡ ﹟ 🖤 ׄ ⬭ *¡ʙʀᴀᴛ ᴀɴɪᴍᴀᴅᴏ!*
 
-*ㅤꨶ〆⁾ ㅤׄㅤ⸼ㅤׄ *͜🖤* ㅤ֢ㅤ⸱ㅤᯭִ*
+import fetch from 'node-fetch';
 
-ׅㅤ𓏸𓈒ㅤׄ *ᴜsᴏ* :: \`#bratv (texto)\`
-ׅㅤ𓏸𓈒ㅤׄ *ᴇᴊᴇᴍᴘʟᴏ* :: \`#bratv hola mundo\`
-ׅㅤ𓏸𓈒ㅤׄ *ɴᴏᴛᴀ* :: ɢᴇɴᴇʀᴀ ꜱᴛɪᴄᴋᴇʀ ᴀɴɪᴍᴀᴅᴏ
+const handler = async (m, { conn, args}) => {
+  if (!args[0]) {
+    return m.reply('🚩 Por favor, proporciona un texto para generar el sticker animado.\n_Ejemplo:.bratsticker Hola mundo_');
+}
 
-> . ﹡ ﹟ ⚡ ׄ ⬭ *ᴀsᴛᴀ-ʙᴏᴛ-ᴍᴅ*`.trim(),
-                    contextInfo: { ...rcanal }
-                }, { quoted: m })
-                await m.react('🕒')
-                const videoBuffer = await fetchStickerVideo(text)
-                const stickerBuffer = await sticker(videoBuffer, null, texto1, texto2)
-                await conn.sendMessage(m.chat, { sticker: stickerBuffer }, { quoted: m })
-                await m.react('✔️')
-                break
-            }
+  const text = args.join(' ');
+  const apiUrl = `https://api.nekorinn.my.id/maker/bratvid?text=${encodeURIComponent(text)}`;
+
+  try {
+    m.reply('⏳ Generando tu sticker animado, por favor espera un momento...');
+
+    const response = await fetch(apiUrl);
+    if (!response.ok) throw new Error(`Error al generar el sticker: ${response.statusText}`);
+
+    const buffer = await response.buffer();
+
+    await conn.sendFile(m.chat, buffer, 'bratsticker.webp', '', m, { asSticker: true});
+} catch (error) {
+    console.error('Error al generar el sticker:', error);
+    m.reply('🚩 Ocurrió un error al generar el sticker animado. Por favor, intenta nuevamente más tarde.');
+}
+};
+
+handler.command = ['bratv', 'stickerbrat'];
+export default handler;
