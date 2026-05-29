f5f032b62351c018dce1599563846a9e2f3dc416
YALLICO OWNER <yallico2024@gmail.com>
2026-05-29 12:19:14 -0500

    Actualizar owner-DeathNote.js

--- plugins/owner-DeathNote.js
+++ plugins/owner-DeathNote.js
@@ -1,18 +1,25 @@
-const handler = async (m, { conn, isROwner, text }) => {
-const datas = global
+import { spawn } from 'child_process'
+let handler = async (m, { conn, isROwner, text }) => {
 
-if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
-const { key } = await conn.sendMessage(m.chat, {text: `🚀🚀`}, {quoted: m})
+if (!process.send) throw '*『✦』Reiniciar: node start.js*\n*『✦』Reiniciar: node index.js*'
+
+if (conn.user.jid == conn.user.jid) {
+
+const { key } = await conn.sendMessage(m.chat, {text: `🗂️ Cargando...`}, {quoted: m})
 await delay(1000 * 1)
-await conn.sendMessage(m.chat, {text: `🚀🚀🚀🚀`, edit: key})
+await conn.sendMessage(m.chat, {text: `📦 Cargando...`, edit: key})
 await delay(1000 * 1)
-await conn.sendMessage(m.chat, {text: `🚀🚀🚀🚀🚀🚀`, edit: key})
-await conn.sendMessage(m.chat, {text: `𝙍𝙚𝙞𝙣𝙞𝙘𝙞𝙖𝙧 | 𝙍𝙚𝙨𝙩𝙖𝙧𝙩`, edit: key})
-process.send('reset')}
-handler.help = ['restart'] 
+await conn.sendMessage(m.chat, {text: `♻️ Cargando...`, edit: key})
+await conn.sendMessage(m.chat, {text: `*『⛏️』Comenzar reinicio completo...*`, edit: key})
+
+process.send('reset')
+} else throw 'eh'
+}
+
+handler.help = ['restart']
 handler.tags = ['owner']
-handler.command = ['restart','reiniciar'] 
+handler.command = ['restart', 'reiniciar'] 
 handler.rowner = true
-export default handler
 
+export default handler
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
