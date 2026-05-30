3766c1a10cbbc59f195ed04cda9a55e167e27a67
YALLICO OWNER <yallico2024@gmail.com>
2026-05-30 09:37:13 -0500

    Actualizar ff-8vs8.js

--- plugins/ff-8vs8.js
+++ plugins/ff-8vs8.js
@@ -1,116 +1,32 @@
+import fg from 'api-dylux' 
 import fetch from 'node-fetch'
 import axios from 'axios'
-
-let handler = async (m, { conn, args}) => {
-  if (!args[0]) throw `
-╭━━━〔 ⚔️ *ARCEUS' VERSUS* ⚔️ 〕━━━┓
-┃
-┃ ⏳ *HORARIO:*
-┃ 🇲🇽 MÉXICO: 
-┃ 🇨🇴 COLOMBIA: 
-┃
-┃ 🎮 *MODALIDAD:*
-┃ 👥 *JUGADORES:* 8 VS 8
-┃
-┃ 🏆 *ESCUADRA A:*
-┃    👑 • 
-┃    ⚡ • 
-┃    ⚡ • 
-┃    ⚡ • 
-┃
-┃ 🏆 *ESCUADRA B:*
-┃    👑 • 
-┃    ⚡ • 
-┃    ⚡ • 
-┃    ⚡ • 
-┃
-┃ 🔄 *RESERVAS:*
-┃    👤 • 
-┃    👤 • 
-┃
-┗━━━━━━━━━━━━━━━━━━━━━━━┛
-`
-
-  const textos = [
-    "👾 Arceus'  Bot: DOMINIO TOTAL",
-    "⚔️ DUELO DE TITANES ACTIVADO",
-    "🚀 SYSTEM KEISTOP: CONFLICTO 8VS8"
-  ]
-  
-  // Imagen oficial para todo el sistema
-  const imgOficial = "https://o.uguu.se/MJVbgdQD.jpg"
-
-  const titulo = textos[Math.floor(Math.random() * textos.length)]
-  
-  let thumbBuffer
-  try {
-    const res = await axios.get(imgOficial, { responseType: 'arraybuffer'})
-    thumbBuffer = Buffer.from(res.data)
-  } catch (err) {
-    thumbBuffer = Buffer.from('')
-  }
-
-  const keistopMsg = {
-    key: {
-      fromMe: false,
-      participant: "0@s.whatsapp.net",
-      remoteJid: "status@broadcast"
-    },
-    message: {
-      orderMessage: {
-        itemCount: 2026,
-        status: 1,
-        message: titulo,
-        footerText: "Arceus'  Bot 👾",
-        thumbnail: thumbBuffer,
-        surface: 2,
-        sellerJid: "0@s.whatsapp.net"
-      }
-    }
-  }
-
-  const caption = `
-┏━━━━〔 👾 *KEISTOP' 8 VS 8* 👾 〕━━━┓
-┃
-┃ ⏳ *HORARIOS:*
-┃ 🇲🇽 MÉXICO: ${args[0]}
-┃ 🇨🇴 COLOMBIA: ${args[0]}
-┃
-┃ 🎮 *MODALIDAD:*
-┃ 👥 *JUGADORES:* 8 VS 8
-┃
-┃ 🔱 *ESCUADRA 1:*
-┃    👑 • 
-┃    ⚔️ • 
-┃    ⚔️ • 
-┃    ⚔️ • 
-┃
-┃ 🔱 *ESCUADRA 2:*
-┃    👑 • 
-┃    ⚔️ • 
-┃    ⚔️ • 
-┃    ⚔️ • 
-┃
-┃ 🚀 *SUPLENTES:*
-┃    👾 • 
-┃    👾 • 
-┃
-┃ 👾 *𝐁𝐲: Arceus'  Bot*
-┗━━━━━━━━━━━━━━━━━━━━━━━┛
-
-📢 *Canal:* https://whatsapp.com/channel/0029Vb5oUp43LdQUVViHwc0ml`.trim()
-
-  await conn.sendMessage(m.chat, {
-    image: { url: imgOficial },
-    caption: caption,
-    mentions: []
-  }, { quoted: keistopMsg })
+let handler = async (m, { conn, args, command, usedPrefix }) => {
+if (!args[0]) throw `
+╰› 8 Versus 8 Ი𐑼
+⊹ ࣪ ˖🕚 🇵🇪  | 🇦🇷   |  🇨🇱 
+૮🩹ა *Encargad@*: 
+˚꒰🏡୭ *Reglas :* 
+˚꒰🆚୭ *Rival :* 
+𓍼   ׅ *Titulares :*
+⚡|  
+⚡|  
+⚡|  
+⚡|  
+⚡|  
+⚡|  
+⚡|  
+⚡|  
+𓍼         ִ  *Suplentes :*
+🐾𑁤  
+🐾𑁤  
+🐾𑁤  
+🐾𑁤  
+` 
 }
-
 handler.help = ['8vs8']
-handler.tags = ['freefire']
-handler.command = /^(vs8|8vs8|masc8)$/i
-handler.group = true
+handler.tags = ['FreeFire']
+handler.command = /^(8vs8)$/i
+handler.group = false
 handler.admin = false
-
 export default handler
